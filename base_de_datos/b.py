1d81d4cd4b70dcdab56a3273f214572b63979f91
Sdaniart <125542776+Sdaniart@users.noreply.github.com>
2024-02-05 15:43:57 +0100

    SQL_database
    Ficheros para la creación de la base de datos

---
+++ base_de_datos/b.py
@@ -0,0 +1,17 @@
+import mysql.connector
+ 
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="jairo",
+    password="1234",
+)
+
+cursor = conexion.cursor()
+usuarios = "usuarios"
+virustotal = "virustotal"
+cursor.execute(f"CREATE DATABASE {virustotal}")
+
+cursor.execute(f"CREATE DATABASE {usuarios}")
+
+cursor.close()
+conexion.close()
---
+++ base_de_datos/c.py
@@ -0,0 +1,31 @@
+import mysql.connector
+ 
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="jairo",
+    password="1234",
+)
+ 
+cursor = conexion.cursor()
+
+usuarios = "usuarios"
+virustotal = "virustotal"
+
+cursor.execute(f"CREATE DATABASE {usuarios}")
+
+conexion.select_db(usuarios)
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS comprobacionn (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(120),
+    apellido VARCHAR(120),
+    passwd VARCHAR(15),
+    departamento_empresa VARCHAR(30)
+)
+"""
+
+cursor.execute(create_table_query)
+
+cursor.close()
+conexion.close()
