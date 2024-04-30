4107fa8b0ef3fa6f54ab2e0064687d4e4e3f7bc2
Sdaniart <125542776+Sdaniart@users.noreply.github.com>
2024-04-30 15:34:55 +0200

    Corrección fichero web a servidor

---
+++ web/archivo_srv.php
@@ -0,0 +1,11 @@
+<?php
+//$targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+$contar = count($_FILES["archivos"]);
+for ($i = 0; $i < $contar; $i++){
+	echo $_FILES["archivos"]["name"][$i] . "<br>";
+	$ruta_destino = 'subidas/' . $_FILES['archivos']['name'][$i];
+   	move_uploaded_file($_FILES['archivos']['tmp_name'][$i], $ruta_destino);
+}
+exec('python3 comprobacion.py');
+?>
+
